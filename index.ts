0821ee615b0a14b823618502037f8506eb36456f
cj484 <ccoreas484@gmail.com>
2023-12-29 18:18:11 -0500

    Update PORT in index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 4000;
+const PORT = 80;
 import { PrismaClient } from "@prisma/client";
 import AddUser from "./Functions/AddUser";
 import Authenticate from "./Functions/Authenticate";
